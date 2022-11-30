7f8141599cef52baa802f247fe123b516cf67156
GUSTAVO DOS SANTOS SILVA <gustavo.ssilva88@senacsp.edu.br>
2022-11-30 17:14:42 -0300

    first commit

---
+++ java.js
@@ -0,0 +1,17 @@
+function bot1(){
+    document.getElementById('azul').classList.add('bot1')
+}
+
+
+function bot2(){
+    document.getElementById('tranparente').classList.add('bot2')
+}
+
+
+function bot3(){
+    document.getElementById('vermelho').classList.add('bot3')
+}
+
+function bot4(){
+    document.getElementById('normal').classList.add('bot4')
+}
